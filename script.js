f39cafeb66fc0e7bc3728498b24b9a2ef893846f
Moyses Josef <145623365+moyses30@users.noreply.github.com>
2024-09-04 21:31:43 -0300

    novo

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let fraseQualquerDepois = 'Essa 2' ;
-fraseQualquerDepois = 'outro valor'
+// const nossaString ='Moyses é ' ;
+// const outraString='Programador';
 
-const outraFrase ='Frase '
+//  console.log(`Voce sabia que o Moyses e ${outraString}`);
 
-console.log(fraseQualquerDepois) ;
+// console.log(nossaString + ' ' + outraString) ;
+
+const string ='concluido';
+
+const numero= 3;
+const numero1 = 2e10 ;
+
+console.log(typeof numero1,numero1) ;
